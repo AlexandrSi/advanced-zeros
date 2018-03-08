636dec571e00f0451eb9eaba9d26d7f54a459842
AlexandrSi <snaiper.bla@gmail.com>
2018-03-08 17:25:35 +0300

    So so

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var n = 0;
+    var x;
+
+    for (var t=2; t<=base; t++) {
+        if (base%t==0&&isSimpleNumb(t)){
+            x=t;
+        }
+    }
+    while(number >= x){
+        number = Math.floor(number / x);
+        n+= number;
+    }
+    return n;
+}
+function isSimpleNumb (numb) {
+        var isNumb = true;
+        for (var j = 2; j<numb; j++){
+            if (numb % j == 0){
+                isNumb = false;
+            }
+        }
+    return isNumb;
 }
